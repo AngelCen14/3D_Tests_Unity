a69135a6807d593e0af2a89ea567b9cbefaf9b0f
AngelCen14 <angelcen1447@gmail.com>
2024-11-19 14:15:13 +0100

    Fix namespace: GameInput.cs

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,67 +1,65 @@
 using System;
 using UnityEngine;
-using static UnityEngine.InputSystem.InputAction;
+using UnityEngine.InputSystem;
 
-namespace Player {
-    public class GameInput : MonoBehaviour {
-        public static GameInput Instance { get; private set; }
+public class GameInput : MonoBehaviour {
+    public static GameInput Instance { get; private set; }
 
-        InputActions _inputActions;
+    InputActions _inputActions;
 
-        public Vector2 MovementInput { get; private set; }
-        public bool Jump {  get; private set; }
+    public Vector2 MovementInput { get; private set; }
+    public bool Jump {  get; private set; }
 
-        public event Action EmotePerformed;
+    public event Action EmotePerformed;
 
-        #region Unity Methods
-        private void Awake() {
-            if (Instance != null && Instance != this) {
-                Destroy(this);
-            } else {
-                Instance = this;
-            }
-            _inputActions = new InputActions();
+    #region Unity Methods
+    private void Awake() {
+        if (Instance != null && Instance != this) {
+            Destroy(this);
+        } else {
+            Instance = this;
         }
+        _inputActions = new InputActions();
+    }
 
-        public void Start() {
-            _inputActions.Player.Move.performed += OnMovePerformed;
-            _inputActions.Player.Move.canceled += OnMoveCancelled;
-            _inputActions.Player.Jump.performed += OnJumpStatusChanged;
-            _inputActions.Player.Jump.canceled += OnJumpStatusChanged;
-            _inputActions.Player.Emote.performed += OnEmotePerformed;
-        }
+    public void Start() {
+        _inputActions.Player.Move.performed += OnMovePerformed;
+        _inputActions.Player.Move.canceled += OnMoveCancelled;
+        _inputActions.Player.Jump.performed += OnJumpStatusChanged;
+        _inputActions.Player.Jump.canceled += OnJumpStatusChanged;
+        _inputActions.Player.Emote.performed += OnEmotePerformed;
+    }
 
-        private void OnEnable() {
-            _inputActions.Enable();
-        }
+    private void OnEnable() {
+        _inputActions.Enable();
+    }
 
-        private void OnDisable() {
-            _inputActions.Disable();
+    private void OnDisable() {
+        _inputActions.Disable();
 
-            _inputActions.Player.Move.performed -= OnMovePerformed;
-            _inputActions.Player.Move.canceled -= OnMoveCancelled;
-            _inputActions.Player.Jump.performed -= OnJumpStatusChanged;
-            _inputActions.Player.Jump.canceled -= OnJumpStatusChanged;
-            _inputActions.Player.Emote.performed -= OnEmotePerformed;
-        }
-        #endregion
+        _inputActions.Player.Move.performed -= OnMovePerformed;
+        _inputActions.Player.Move.canceled -= OnMoveCancelled;
+        _inputActions.Player.Jump.performed -= OnJumpStatusChanged;
+        _inputActions.Player.Jump.canceled -= OnJumpStatusChanged;
+        _inputActions.Player.Emote.performed -= OnEmotePerformed;
+    }
+    #endregion
 
-        #region Move Events
-        private void OnMovePerformed(CallbackContext context) {
-            MovementInput = context.ReadValue<Vector2>().normalized;
-        }
+    #region Move Events
+    private void OnMovePerformed(InputAction.CallbackContext context) {
+        MovementInput = context.ReadValue<Vector2>().normalized;
+    }
 
-        private void OnMoveCancelled(CallbackContext callback) {
-            MovementInput = Vector2.zero;
-        }
-        #endregion
+    private void OnMoveCancelled(InputAction.CallbackContext callback) {
+        MovementInput = Vector2.zero;
+    }
+    #endregion
 
-        private void OnJumpStatusChanged(CallbackContext context) {
-            Jump = context.ReadValueAsButton();
-        }
+    private void OnJumpStatusChanged(InputAction.CallbackContext context) {
+        Jump = context.ReadValueAsButton();
+    }
 
-        private void OnEmotePerformed(CallbackContext callback) {
-            EmotePerformed?.Invoke();
-        }
+    private void OnEmotePerformed(InputAction.CallbackContext callback) {
+        EmotePerformed?.Invoke();
     }
 }
